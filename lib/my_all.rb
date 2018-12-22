1a4996bc37fc0ffa4942669cb82dd0e11961f95e
Sarah Paz <sarah.paz@gmail.com>
2018-12-22 00:15:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,6 +8,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-end
 
-my_all?([1, 2, 3]) {|i| i < 2}
+  if  block_return_values.include?(false)
+    false
+  else
+    true
+  end
+end
